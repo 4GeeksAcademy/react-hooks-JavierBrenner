fd7c9c22d084a7e492cae4b2436012942c2b170d
JavierBrenner <javibrenner18@gmail.com>
2026-04-22 16:40:46 +0000

    semaforo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	const [activeColor, setActiveColor] = useState("");
+	const [visible, setVisible] = useState(false);
+
+	const changeColor = () => {
+
+		if (activeColor === "red") {
+			setActiveColor("yellow");
+		} else if (activeColor === "yellow") {
+			setActiveColor("green");
+		} else if (activeColor === "green") {
+			if (visible) {
+				setActiveColor("purple");
+			} else {
+				setActiveColor("red");
+			}
+		} else if (activeColor === "purple") {
+			setActiveColor("red");
+		} else {
+			setActiveColor("red");
+		}
+	};
+
+	const getShineClass = (color) => {
+		if (activeColor === color) {
+			return "shine";
+		} else {
+			return "";
+		}
+	};
+
+	//-------------- crear el div purple, 
+	// que cambie el estado de false a true para mostrar el div. --
+
+	return (
+		<>
+			<div className="text-center mt-5">
+				<div style={{ background: "black", width: "100px", padding: "10px", margin: "0 auto", borderRadius: "10px" }}>
+					<div
+						onClick={() => setActiveColor("red")}
+						className={`red light ${getShineClass("red")}`}
+					></div>
+					<div
+						onClick={() => setActiveColor("yellow")}
+						className={`yellow light ${getShineClass("yellow")}`}
+					></div>
+					<div
+						onClick={() => setActiveColor("green")}
+						className={`green light ${getShineClass("green")}`}
+					></div>
+					{visible && (<div
+						onClick={() => setActiveColor("purple")}
+						className={`purple light ${getShineClass("purple")}`}
+					></div>)}
+
+				</div>
+				<div style={{ background: "black", width: "10px", height: "300px", margin: "0 auto" }}></div>
+				<button onClick={() => setVisible(!visible)}>
+					{visible ? "Quitar Púrpura" : "Añadir Púrpura"}
+				</button>
+				<button className="btn btn-primary m-2" onClick={changeColor}>
+					Next color
+				</button>
+			</div>
+		</>
 	);
 };
 
